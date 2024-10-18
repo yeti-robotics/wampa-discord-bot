3409450e266fe346fd3b02267e7a13c4ee200f91
Sam Perlmutter <sam.perlmutter227@gmail.com>
2024-10-18 12:12:03 -0400

    add filter for 14 day old messages when clearing channel

--- src/command.rs
+++ src/command.rs
@@ -1,5 +1,6 @@
 use std::env;
 
+use chrono::{TimeDelta, Utc};
 use serenity::all::{EditMember, GetMessages};
 use serenity::prelude::*;
 use serenity::model::{ channel::Message, id::RoleId };
@@ -27,22 +28,27 @@ impl Command {
             Command::Name(name) => {
                 println!("{:#?}", msg);
                 let guild_id  = msg.guild_id.ok_or(WampaError::InternalServerError("Error finding guild id".to_string()))?;
-                guild_id.edit_member(&ctx.http, msg.author.id, EditMember::new().nickname(name)).await?;
                 println!("{:#?}", guild_id);
 
                 if msg.channel_id.get() == env::var("WELCOME_CHANNEL_ID")?.parse::<u64>()? {
-                    let mut msgs = msg.channel_id.messages(&ctx.http, GetMessages::new().before(msg.id)).await?
-                        .iter()
-                        .filter(|m| m.content.contains(&msg.author.id.to_string()) || &m.author.id == &msg.author.id)
-                        .cloned()
-                        .collect::<Vec<Message>>();
-                    msgs.push(msg.clone());
-                    msg.channel_id.delete_messages(&ctx.http, msgs).await?;
                     let roles = vec![
                         RoleId::new(env::var("MEMBER_ROLE_ID")?.parse::<u64>()?),
                         RoleId::new(env::var("ROOKIE_ROLE_ID")?.parse::<u64>()?)
                     ];
+                    let is_msg_deletable = |msg: &Message| -> bool {
+                        msg.timestamp.timestamp_millis() + TimeDelta::days(14).num_milliseconds() > Utc::now().timestamp_millis()
+                    };
+
+                    guild_id.edit_member(&ctx.http, msg.author.id, EditMember::new().nickname(name)).await?;
                     guild_id.edit_member(&ctx.http, msg.author.id, EditMember::new().roles(roles)).await?;
+
+                    let mut msgs = msg.channel_id.messages(&ctx.http, GetMessages::new().before(msg.id)).await?
+                        .iter()
+                        .filter(|m| is_msg_deletable(m) && (m.content.contains(&msg.author.id.to_string()) || m.author.id == msg.author.id))
+                        .cloned()
+                        .collect::<Vec<Message>>();
+                    msgs.push(msg.clone());
+                    msg.channel_id.delete_messages(&ctx.http, msgs).await?;
                 }
             }
         }
